0cbb4ea9955186122981e0116c79d6039f711226
thebrandconsultant <james@thebrandconsultant.co.za>
2020-04-09 09:12:32 +0200

    Reports Page

---
+++ src/pages/reports/2020-03-25-how-technology-is-making-it-easier-to-manipulate-people-business-day.md
@@ -0,0 +1,15 @@
+---
+templateKey: report-post
+title: How technology is making it easier to manipulate people | Business Day
+date: 2020-03-25T15:16:33.316Z
+description: >-
+  Never before in human history has the ability to manipulate people been as
+  easy and widespread as it is now. In the past, the more homogeneous
+  communities were, the more vulnerable people were to propagandistic rhetoric
+  and messaging. 
+featuredpost: false
+featuredimage: https://avatars2.githubusercontent.com/u/9041049?s=88&u=d8822b7d2297c639c4b761a0e8c884a51477926a&v=4
+tags:
+  - Media Coverage
+---
+[Read the full article >](https://www.businesslive.co.za/bd/opinion/2018-11-26-how-technology-is-making-it-easier-to-manipulate-people/)
---
+++ src/pages/reports/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import Layout from '../../components/Layout'
+import ReportRoll from '../../components/ReportRoll'
+
+export default class ReportIndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: `url('/img/news.jpg')`,
+          }}
+        >
+          <h1
+            className="blogTitleBox has-text-weight-bold is-size-1"
+          >
+            Reports
+          </h1>
+        </div>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <ReportRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
+}
